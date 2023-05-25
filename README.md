c1ead894bdae2a703b3dd870212eb862c9db2903
Mozhaev Alexandr <mo.sasha123@yandex.ru>
2023-05-25 12:22:16 +0300

    del garbage

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-TODO: Add more libraries
 
-TODO: Write tests
--- tests/methods/test_devision.py
+++
@@ -1,6 +0,0 @@
-from math import sqrt
-import pytest
-
-@pytest.mark.parametrize("a, expected_result", [(4,2),(5,2)])
-def test_division_good(a, expected_result):
-    assert sqrt(a) == expected_result
